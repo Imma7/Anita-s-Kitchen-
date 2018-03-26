7ee412de7fdb00f263d1ff6ad700ebb9e8488857
Imma7 <immamugambi@gmail.com>
2018-03-26 03:42:04 +0300

    Added color to fonts

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+jQuery(document).ready(function() {  
+    jQuery(".btn-subscribe").click(function() {
+      alert("immamugambi@gmail.com has been successfully added to our email list. Thank you.");
+});

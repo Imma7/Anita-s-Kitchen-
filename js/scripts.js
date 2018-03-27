30f7b1aa82c3ef15a5dffeefc0acb8b40b74d74b
Imma7 <immamugambi@gmail.com>
2018-03-27 10:13:34 +0300

    Align burger logo to Menu text

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,7 @@
-jQuery(document).ready(function() {  
-    jQuery(".btn-subscribe").click(function() {
-      alert("immamugambi@gmail.com has been successfully added to our email list. Thank you.");
-});
+// jQuery(document).ready(function() {  
+//     jQuery(".btn-subscribe").click(function() {
+//       alert("immamugambi@gmail.com has been successfully added to our email list. Thank you.");
+
+function myFunction(x) {
+  x.classList.toggle("change");
+ }
